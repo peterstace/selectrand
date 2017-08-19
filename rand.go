0d765c7ea4b0e1e33a6212bf3a41ab8ba1edbee2
Peter Stace <peter@dgraph.io>
2017-08-19 10:44:44 +1000

    Initial commit

---
+++ rand.go
@@ -0,0 +1,32 @@
+package selectrnd
+
+var (
+	c1 = make(chan struct{})
+	c2 = make(chan struct{})
+)
+
+func init() {
+	close(c1)
+	close(c2)
+}
+
+func b() byte {
+	var b byte
+	for i := uint(0); i < 8; i++ {
+		select {
+		case <-c1:
+			b = b | (1 << i)
+		case <-c2:
+		}
+	}
+	return b
+}
+
+type SelectSource struct{}
+
+func (SelectSource) Int63() int64 {
+	return 0 // TODO
+}
+
+func (SelectSource) Seed(int64) {
+}
